33aec8c9d65807237a367819452bb38c5495924e
daniyal-haider <imksdh@telus.net>
2013-07-30 15:29:58 -0700

    live project

---
+++ UnitTesting/NUnitConsoleApplication01/TestProject/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnitConsoleApplication01;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            MathsHelper helper = new MathsHelper();
+            int result = helper.Add(20, 10);
+            Assert.AreEqual(30, result);
+        }
+
+       [TestMethod]
+        public void SubtractTest()
+        {
+            MathsHelper helper = new MathsHelper();
+            int result = helper.Subtract(20, 10);
+            Assert.AreEqual(10, result);
+        }
+    }
+}
